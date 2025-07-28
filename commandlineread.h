eea56d3857dda1ba64bc7a5507811c3d785d1950
Colin Iverson <coliniverson07@gmail.com>
2025-07-27 23:31:56 -0500

    comenting commandlineread

--- commandlineread.h
+++ commandlineread.h
@@ -9,9 +9,13 @@
 
 class Reader {
 public:
+    // contents of teh command line
     std::vector<std::string> commandline;
+    // constructor takes the command line arguments straight through
     Reader(int argc, char* argv[]);
+    // finds a specific argument, e.g. finds a -f to trigger something
     bool find_member(std::string str);
+    // finds an argument with it's value, e.g. -a RETURNS_THIS
     std::string find_member_next(std::string str);
 };
 
