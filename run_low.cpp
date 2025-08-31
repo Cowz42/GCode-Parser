a1a95fdc7d45b732e17c60c73ac8c7ea45fcbd2b
Colin Iverson <coliniverson07@gmail.com>
2025-08-30 20:32:00 -0500

    Switched implementation of run_low for easier outside access

--- run_low.cpp
+++ run_low.cpp
@@ -4,6 +4,6 @@
 #include <iostream>
 
 
-void Machine::run_low() {
+void Machine::run_low(float in_x, float in_y, float in_z, float s_x, float s_y, float s_z) {
     std::cout << "this function still needs to be written\n";
 };
